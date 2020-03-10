a1b1deed8abff5f8c7c1df450bdbc5eb46b07b0d
Ewa98 <komcia1234@gmail.com>
2020-03-10 23:51:59 +0100

    final version Excercise 3.1

--- Zad3/Exercise1/OpenNLP.java
+++ Zad3/Exercise1/OpenNLP.java
@@ -1,9 +1,22 @@
+import opennlp.tools.chunker.ChunkerME;
+import opennlp.tools.chunker.ChunkerModel;
 import opennlp.tools.langdetect.Language;
 import opennlp.tools.langdetect.LanguageDetector;
 import opennlp.tools.langdetect.LanguageDetectorME;
 import opennlp.tools.langdetect.LanguageDetectorModel;
+import opennlp.tools.lemmatizer.DictionaryLemmatizer;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinder;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.stemmer.PorterStemmer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.Span;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,12 +45,12 @@ public class OpenNLP {
 
 		//languageDetection();
 		//tokenization();
-        sentenceDetection();
-		// posTagging();
-		// lemmatization();
-		// stemming();
-		// chunking();
-		// nameFinding();
+        //sentenceDetection();
+		//posTagging();
+		//lemmatization();
+		//stemming();
+		//chunking();
+		nameFinding();
 	}
 
 	private void languageDetection() throws IOException
@@ -101,60 +114,114 @@ public class OpenNLP {
 
 	private void sentenceDetection() throws IOException
     {
+		File modelFile = new File(SENTENCE_MODEL);
+		SentenceModel model = new SentenceModel(modelFile);
+		SentenceDetectorME sentenceDetectorME = new SentenceDetectorME(model);
+
 		String text = "";
-		text = "Hi. How are you? Welcome to OpenNLP. "
-				+ "We provide multiple built-in methods for Natural Language Processing.";
-		/*text = "Hi. How are you?! Welcome to OpenNLP? "
-				+ "We provide multiple built-in methods for Natural Language Processing.";
-		text = "Hi. How are you? Welcome to OpenNLP.?? "
-				+ "We provide multiple . built-in methods for Natural Language Processing.";
+		/*text = "Hi. How are you? Welcome to OpenNLP. "
+				+ "We provide multiple built-in methods for Natural Language Processing."; */
+		text = "Hi. How are you?! Welcome to OpenNLP? "
+				+ "We provide multiple built-in methods for Natural Language Processing.";	 //first three in one line
+		/*text = "Hi. How are you? Welcome to OpenNLP.?? "
+				+ "We provide multiple . built-in methods for Natural Language Processing."; //last sentence in many lines
 		text = "The interrobang, also known as the interabang (often represented by ?! or !?), "
 				+ "is a nonstandard punctuation mark used in various written languages. "
 				+ "It is intended to combine the functions of the question mark (?), or interrogative point, "
 				+ "and the exclamation mark (!), or exclamation point, known in the jargon of printers and programmers as a \"bang\". ";*/
 
+		String[] sentDetect = sentenceDetectorME.sentDetect(text);
+		double [] prob = sentenceDetectorME.getSentenceProbabilities();
+
+		for (String s : sentDetect) {
+			System.out.println(s);
+		}
+
+		for (double d: prob) {
+			System.out.println(d);
+		}
 	}
 
 	private void posTagging() throws IOException {
+		File modelFile = new File(POS_MODEL);
+		POSModel model = new POSModel(modelFile);
+		POSTaggerME posTaggerME = new POSTaggerME(model);
+
 		String[] sentence = new String[0];
-		sentence = new String[] { "Cats", "like", "milk" };
-		/*sentence = new String[]{"Cat", "is", "white", "like", "milk"};
-		sentence = new String[] { "Hi", "How", "are", "you", "Welcome", "to", "OpenNLP", "We", "provide", "multiple",
-				"built-in", "methods", "for", "Natural", "Language", "Processing" };
-		sentence = new String[] { "She", "put", "the", "big", "knives", "on", "the", "table" };*/
+		//sentence = new String[] { "Cats", "like", "milk" };		//NNS IN NN
+		//sentence = new String[]{"Cat", "is", "white", "like", "milk"};		//NNP VBZ JJ IN NN
+		//sentence = new String[] { "Hi", "How", "are", "you", "Welcome", "to", "OpenNLP", "We", "provide", "multiple",
+				//"built-in", "methods", "for", "Natural", "Language", "Processing" };			//NNP WRB VBP PRP VB TO VB PRP VB JJ JJ NNS IN JJ NN VBG
+		sentence = new String[] { "She", "put", "the", "big", "knives", "on", "the", "table" };	//PRP VBD DT JJ NNS IN DT NN
+
+		String [] posTags = posTaggerME.tag(sentence);
+		for (String pos : posTags) {
+			System.out.println(pos);
+		}
 	}
 
 	private void lemmatization() throws IOException
     {
+		File modelFile = new File(LEMMATIZER_DICT);
+		DictionaryLemmatizer lemmatizer = new DictionaryLemmatizer(modelFile);
+
 		String[] text = new String[0];
 		text = new String[] { "Hi", "How", "are", "you", "Welcome", "to", "OpenNLP", "We", "provide", "multiple",
-				"built-in", "methods", "for", "Natural", "Language", "Processing" };
+				"built-in", "methods", "for", "Natural", "Language", "Processing" };		//po dodaniu - blad, wyjscie poza slownik
 		String[] tags = new String[0];
 		tags = new String[] { "NNP", "WRB", "VBP", "PRP", "VB", "TO", "VB", "PRP", "VB", "JJ", "JJ", "NNS", "IN", "JJ",
 				"NN", "VBG" };
 
+		//słówko "are" lemat: be, steam: "ar"
+		//zmiana form wyrazów lub ich znaczenia
+
+		String[] lematList = lemmatizer.lemmatize(text, tags);
+		for (String s: lematList) {
+			System.out.println(s);
+		}
 	}
 
 	private void stemming()
     {
+		PorterStemmer stemmer = new PorterStemmer();
+
 		String[] sentence = new String[0];
 		sentence = new String[] { "Hi", "How", "are", "you", "Welcome", "to", "OpenNLP", "We", "provide", "multiple",
-				"built-in", "methods", "for", "Natural", "Language", "Processing" };
+				"built-in", "methods", "for", "Natural", "Language", "Processing", "None" };	//po dodaniu - przejscie bez parsowania słowa w żaden sposob
 
+		for (String s: sentence) {
+			System.out.println(stemmer.stem(s));
+		}
 	}
 	
 	private void chunking() throws IOException
     {
+		File modelFile = new File(CHUNKER_MODEL);
+		ChunkerModel model = new ChunkerModel(modelFile);
+		ChunkerME chunkerME = new ChunkerME(model);
+
 		String[] sentence = new String[0];
 		sentence = new String[] { "She", "put", "the", "big", "knives", "on", "the", "table" };
 
 		String[] tags = new String[0];
 		tags = new String[] { "PRP", "VBD", "DT", "JJ", "NNS", "IN", "DT", "NN" };
 
+		String[] chunk = chunkerME.chunk(sentence, tags);
+
+		//I-	inside the chunk
+		//B-	inside the chunk, preceding word is part of a different chunk
+		//4 rodzaje: B-NP, B-VP, I-NP, B-PP
+		for (String ch :chunk) {
+			System.out.println(ch);
+		}
 	}
 
 	private void nameFinding() throws IOException
     {
+		File modelFile = new File(NAME_MODEL);
+		TokenNameFinderModel model = new TokenNameFinderModel(modelFile);
+		NameFinderME nameFinderME = new NameFinderME(model);
+
 		String text = "he idea of using computers to search for relevant pieces of information was popularized in the article "
 				+ "As We May Think by Vannevar Bush in 1945. It would appear that Bush was inspired by patents "
 				+ "for a 'statistical machine' - filed by Emanuel Goldberg in the 1920s and '30s - that searched for documents stored on film. "
@@ -165,6 +232,26 @@ public class OpenNLP {
 				+ "well on small text corpora such as the Cranfield collection (several thousand documents). Large-scale retrieval systems, "
 				+ "such as the Lockheed Dialog system, came into use early in the 1970s.";
 
+		String[] tokens = WhitespaceTokenizer.INSTANCE.tokenize(text);
+		Span[] spans = nameFinderME.find(tokens);
+		String[] names = Span.spansToStrings(spans, tokens);
+
+		for (String name : names) {
+			System.out.println(name);
+		}
+
+
+		modelFile = new File(ENTITY_XYZ_MODEL);			//This model finds dates
+		model = new TokenNameFinderModel(modelFile);
+		nameFinderME = new NameFinderME(model);
+
+		tokens = WhitespaceTokenizer.INSTANCE.tokenize(text);
+		spans = nameFinderME.find(tokens);
+		names = Span.spansToStrings(spans, tokens);
+
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 
 }
